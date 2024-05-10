e6885b9cefbfb60fda95d52ba3060cb2ba15fb51
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2024-05-10 13:11:11 +0200

    Noms de jours

--- ig-timer/src/main.cpp
+++ ig-timer/src/main.cpp
@@ -2,10 +2,13 @@
 #include<string>
 #include<cstdint>
 
+const std::string days_name[7] = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
+
 typedef struct {
   int64_t year;
   uint16_t month;
   uint16_t day;
+  std::string name;
 } DateObject;
 
 DateObject days_to_date(uint16_t days) {
@@ -13,13 +16,18 @@ DateObject days_to_date(uint16_t days) {
   uint16_t month = 1;
   uint16_t date = 1;
 
+  uint16_t day_name_index = 0;
+
   uint16_t daysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
   while(days > 0) {
     uint16_t remainingDaysInMonth = daysInMonth[month - 1] - date + 1;
 
+
     if(days > remainingDaysInMonth) {
       days -= remainingDaysInMonth;
+      day_name_index += remainingDaysInMonth;
+      day_name_index %= 7;
       date = 1;
       month++;
 
@@ -29,14 +37,18 @@ DateObject days_to_date(uint16_t days) {
       }
     } else {
       date += days - 1;
+      day_name_index += days - 1;
+      day_name_index %= 7;
       days = 0;
     }
+
   }
 
   DateObject obj;
   obj.year = year;
   obj.month = month;
   obj.day = date;
+  obj.name = days_name[day_name_index];
 
   return obj;
 };
@@ -44,13 +56,13 @@ DateObject days_to_date(uint16_t days) {
 std::string month_number_to_string(uint16_t number) {
   std::string months[12] = {
     "Janvier",
-    "Février",
+    "Fevrier",
     "Mars",
     "Avril",
     "Mai",
     "Juin",
     "Juillet",
-    "Août",
+    "Aout",
     "Septembre",
     "Octobre",
     "Novembre",
@@ -70,6 +82,8 @@ int32_t main(int32_t argc, char** argv) {
   uint16_t days = static_cast<uint16_t>(strtoul(day_as_string, NULL, 0));
 
   DateObject date = days_to_date(days);
+
+  std::cout << date.name << " ";
 
   if(date.day == 1)
     std::cout << "1er ";
