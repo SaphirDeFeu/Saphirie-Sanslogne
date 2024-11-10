90c315c5077a53402f9e51149b8f527c3e170624
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2024-11-10 13:11:18 +0100

    messages

--- development/minigamelolcow/src/main/java/io/github/saphirdefeu/minigamelolcow/discordimpl/eventlisteners/MessageReceived.java
+++ development/minigamelolcow/src/main/java/io/github/saphirdefeu/minigamelolcow/discordimpl/eventlisteners/MessageReceived.java
@@ -27,7 +27,9 @@ public class MessageReceived extends ListenerAdapter {
         HashMap<String, String[]> messages = new HashMap<>();
         messages.put("random", new String[]{
                 "Union fédérative de Saphirie-Sanslogne N°1 !!!!!!!! <:union:1268165799328223283> <:union:1268165799328223283>",
-                "Vérifies blogpopulaire.sa y a totalement un nouveau poste"
+                "Vérifies blogpopulaire.sa y a totalement un nouveau poste",
+                "# FROM THE SCREEN :tv: TO THE RING :boxing_glove: TO THE PEN :pen: TO THE KING :crown: WASSSSHHHHHHIIIIINNGGGGGGG :crossed_swords:",
+                "Comme KSI* le dit:\n> BUT IM COOOLLLDLDDDDDDLDLDLDL :cold_face: :cold_face:"
         });
         messages.put("finance", new String[]{
                 "Je suis actuellement en train d'étudier comment je pourrais augmenter mon rendement",
@@ -40,21 +42,27 @@ public class MessageReceived extends ListenerAdapter {
                 "Safŏr Dẽfẽ maintenant que le SAM est au pouvoir : :rainbow_flag::x:"
         });
         messages.put("saphirie", new String[]{
-                "Imagine habiter en Saphirie (common saphiriq L)",
+                "Imagine habiter en Saphirie (common saphiriq W)",
                 "Pov la saphirie quand ils ont plus de thune au gouvernement : :speaking_head:",
-                "Je propose qu'on refait la frontière : donnez B-131 aux Sanslogniens et donnez Colonz aux saphiriqs"
+                "Pov la saphirie quand ils se rendent compte qu'ils peuvent juste pomper de la thune dans l'industrie : :fire: :fire:",
+                "Je propose qu'on refait la frontière : donnez B-131 aux Sanslogniens et donnez Colonz aux saphiriqs",
+                "J'ai juré j'ai jamais vu un jour en saphirie sans qu'il y ait de la propagande électorale",
+                "Les gars on a pas fait Lone pour qu'on se retrouve À NOUVEAU avec des tyrans au pouvoir!"
         });
         messages.put("sanslogne", new String[]{
                 "Imagine habiter en Sanslogne (common sanslogne W)",
+                "Pov la sanslogne après avoir construit une banque : :100: $$$",
                 "Pov la sanslogne quand ils ont du pétrole dans le sol (ils vont plus pouvoir respirer) : :money_mouth:",
-                "Je propose Libération au n°14"
+                "Je propose Libération du n°14",
+                "Les théocrates ont déjà théoriquement gagnés... (opinion polémique ici)",
+                "Venez on va à la plage d'Athena ce soir!... Attends... t'as pas oublié la protection anti-théocrates?"
         });
 
         int[] counts = {
-                countOccurrences(raw, "thune", "finance", "argent", "monnaie", "money", "$", "€"),
+                countOccurrences(raw, "thune", "finance", "argent", "monnaie", "money", "$", "€", "£", "₽"),
                 countOccurrences(raw, "lgbt", "gay", "lesbienne", "bisexuel", "trans", "sexualité", "aro", "ace", "asexuel"),
                 countOccurrences(raw, "saphirie", "saphiriq", "saforga", "safŏrga", "safiorga"),
-                countOccurrences(raw, "sanslogne", "sanslognien", "athena"),
+                countOccurrences(raw, "sanslogne", "sanslognien", "athena")
         };
         // END ALGORITHM SELECTION MODEL
 
@@ -73,18 +81,18 @@ public class MessageReceived extends ListenerAdapter {
         for(int i = 0; i < probabilities.length; i++) {
             double prob = probabilities[i];
             String _t = types.get(i);
-            Logger.debug(String.format("Probability of message type %s: %f", _t, prob));
+            Logger.debug(String.format("[DISCORD] Probability of message type %s: %f", _t.toUpperCase(), prob));
         }
 
         OptionalDouble optionalMax = Arrays.stream(probabilities).max();
         double max = 0;
         if(optionalMax.isPresent()) max = optionalMax.getAsDouble();
-        Logger.debug(String.format("Max probability: %f", max));
+        Logger.debug(String.format("[DISCORD] Max probability: %f", max));
         if(max != 0) {
             List<Double> list = Arrays.stream(probabilities).boxed().toList();
             int index = list.indexOf(max);
             if(index != -1) type = types.get(index);
-            Logger.debug(String.format("Index of max probability: %d - Selected type of response: %s", index, type));
+            Logger.debug(String.format("[DISCORD] Index of max probability: %d - Selected type of response: %s", index, type.toUpperCase()));
         }
 
         Random rng = new Random();
