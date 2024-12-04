4704c882b472a6d4bdeca73a153425cf17a38eca
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2024-12-04 20:25:55 +0100

    ???

--- development/parlement-gen/src/App.vue
+++ development/parlement-gen/src/App.vue
@@ -22,25 +22,44 @@ header {
   z-index: 100;
   top: 0;
   left: 0;
-  margin: 0;
-  padding: 0;
+  margin: 1vh;
   border: none;
-  height: 6vh;
-  width: 100vw;
-  display: fixed;
-  flex-direction: row;
-  background-color: #dddddd;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
-header img {
-  margin-right: 5vw;
-  height: 6vh;
-  aspect-ratio: inherit;
-  border: 1px solid red;
+header a {
+  margin-top: 1vw;
+  z-index: 101;
+  top: 0;
+  left: 0;
+  padding: 10px;
+  width: 15vw;
   border-radius: 4px;
 }
 
+header img {
+  width: 12vw;
+  padding: 2px;
+  aspect-ratio: inherit;
+  border-radius: 4px;
+
+  animation-name: select-route-inverse;
+  animation-duration: 0.3s;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
+}
+
+header img:hover {
+  animation-name: select-route;
+  animation-duration: 0.3s;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
+}
+
 main {
+  z-index: 0;
   margin: 0;
   padding: 0;
   position: absolute;
@@ -50,5 +69,33 @@ main {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+
+@keyframes select-route {
+  0% {
+    width: 12vw;
+    padding: 2px;
+    background: blue;
+  }
+
+  100% {
+    width: calc(12vw * 1.1);
+    padding: calc(2px * 1.1);
+    background: red;
+  }
+}
+
+@keyframes select-route-inverse {
+  0% {
+    width: calc(12vw * 1.1);
+    padding: calc(2px * 1.1);
+    background: red;
+  }
+
+  100% {
+    width: 12vw;
+    padding: 2px;
+    background: blue;
+  }
 }
 </style>
