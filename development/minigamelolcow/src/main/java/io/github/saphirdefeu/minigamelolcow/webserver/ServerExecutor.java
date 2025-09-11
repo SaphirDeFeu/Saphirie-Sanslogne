b1bdcbd30f471c22497452d66248111e04954bac
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2025-09-11 22:30:19 +0200

    modified webserver details

--- development/minigamelolcow/src/main/java/io/github/saphirdefeu/minigamelolcow/webserver/ServerExecutor.java
+++ development/minigamelolcow/src/main/java/io/github/saphirdefeu/minigamelolcow/webserver/ServerExecutor.java
@@ -3,7 +3,6 @@ package io.github.saphirdefeu.minigamelolcow.webserver;
 import io.github.saphirdefeu.minigamelolcow.Logger;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ServerExecutor implements Runnable {
@@ -11,7 +10,7 @@ public class ServerExecutor implements Runnable {
     @Override
     public void run() {
         try {
-            String command = "./plugins/minigamelolcow/libs/nodejs/node.exe ./plugins/minigamelolcow/webserver/index.js";
+            String command = "./plugins/minigamelolcow/libs/nodejs/node.exe ./plugins/minigamelolcow/webserver/index.js 91.197.6.118 31252";
 
             ProcessBuilder builder = new ProcessBuilder(command.split(" "));
             Process process = builder.start();
