e2b3d0c5c653f164ec0b60e7edb8840fadb529f1
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2025-09-12 16:09:48 +0200

    update process

--- development/minigamelolcow_web/index.js
+++ development/minigamelolcow_web/index.js
@@ -1,4 +1,17 @@
-const PORT = process.argv[3];
-const HOST = process.argv[2];
+const child_proc = require("child_process");
 
-console.log(HOST + ":" + PORT);
+console.log(process.argv);
+const proc = child_proc.exec("npm i", (err, stdout, stderr) => {
+  if (err) console.log(err);
+  if (stderr) console.log(stderr);
+  console.log(stdout);
+});
+
+proc.on("exit", main);
+
+function main() {
+  const PORT = process.argv[3];
+  const HOST = process.argv[2];
+
+  console.log(HOST + ":" + PORT);
+}
