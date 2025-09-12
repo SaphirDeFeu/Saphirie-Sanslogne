ac2292637be48c98ed46907aa77ad8cf006a2136
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2025-09-12 21:37:30 +0200

    index page for websites

--- development/minigamelolcow_web/index.js
+++ development/minigamelolcow_web/index.js
@@ -3,5 +3,17 @@ const express = require("express");
 
 const PORT = process.argv[3];
 const HOST = process.argv[2];
+const app = express();
 
-console.log(HOST + ":" + PORT);
+app.use(express.static("public"));
+app.use((req, res, next) => {
+  const time = new Date(Date.now()).toLocaleString("fr");
+  const ip = req.socket.remoteAddress || req.headers["x-forwarded-for"];
+  console.log(`[REQ] [${time}] ${ip} ${req.method} ${req.url}`);
+
+  next();
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Listening on ${HOST}:${PORT}`);
+});
