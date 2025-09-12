efe971237ba4886ac57f689e9f6beaebec6f1b5e
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2025-09-12 21:13:05 +0200

    added express & moved npm logic
    npm install is now handled by the parent program.

--- development/minigamelolcow_web/index.js
+++ development/minigamelolcow_web/index.js
@@ -1,17 +1,7 @@
 const child_proc = require("child_process");
+const express = require("express");
 
-console.log(process.argv);
-const proc = child_proc.exec("npm i", (err, stdout, stderr) => {
-  if (err) console.log(err);
-  if (stderr) console.log(stderr);
-  console.log(stdout);
-});
+const PORT = process.argv[3];
+const HOST = process.argv[2];
 
-proc.on("exit", main);
-
-function main() {
-  const PORT = process.argv[3];
-  const HOST = process.argv[2];
-
-  console.log(HOST + ":" + PORT);
-}
+console.log(HOST + ":" + PORT);
